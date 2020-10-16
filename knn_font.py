354bbbf701b36b2d9837634b4b607fed21f17312
win-wdkang <776341482@qq.com>
2020-10-16 16:07:38 +0800

    my font

--- knn_font.py
+++ knn_font.py
@@ -2,10 +2,41 @@ import numpy as np
 import operator
 from fontTools.ttLib import TTFont
 
+# inArr 传入的font数据(经过构造)
+# datingDataMat 训练集的数据
+# datingLabels 训练集的标签
+# classify0(inArr, datingDataMat, datingLabels, 1)
 def classify0(inX, dataSet, labels, k):
-    #KNN
+    # KNN
+    
+    # dataSet 本地的 fontdata.txt 只有30条数据
+    # print(dataSet)
+    # 所以 shape[0] 为 样本的个数
     dataSetSize = dataSet.shape[0]
+    # print(dataSetSize)
+    # print("==============================")
+
+    # np.tile()
+    # 1.沿X轴复制
+    # 2.XY轴都复制，或只沿着Y轴复制的方法
+
+    # 假如
+    # inX = [1, 2, 3, 4, 5]
+    # data = np.tile(inX, 1)
+    # data --> array([1, 2, 3, 4, 5])
+    
+    # data2 = np.tile(inX, 2)
+    # data2 --> array([1, 2, 3, 4, 5, 1, 2, 3, 4, 5])
+    
+    # data3 = np.tile(inX, (2, 1))
+    # data3 --> array([[1, 2, 3, 4, 5],[1, 2, 3, 4, 5]])
+    
+    # data4 = np.tile(inX, (2, 2))
+    # data4 --> array([[1, 2, 3, 4, 5, 1, 2, 3, 4, 5],[1, 2, 3, 4, 5, 1, 2, 3, 4, 5]])
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    print(diffMat)
+    print(diffMat.shape)
+    print("====================================")
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
@@ -55,10 +86,25 @@ def file2matrix():
     return returnMat,classLabelVector
 
 def classifyPerson(font):
-    #输出结果
+    # 传进来的font 是字体的坐标信息
+    # print("======== font =======")
+    # print(font)
+    # print("=====================")
+
+    # 生成一个新的 200大小的空间
     returnMats = np.zeros([200])
+    
+    # 把 font 的内容 存放到上边生成的空间中去
     returnMats[:len(font)] = font
+    # print("======= resturnMats =======")
+    # print(returnMats)
+    # print("===========================")
+    
+    # datingDataMat数据 和 datingLabels标签
+    # file2matrix() 是把数据集导入进来
     datingDataMat, datingLabels = file2matrix()
     inArr = returnMats
+    # 调用 classify0 训练数据得出结果
     classifierResult = classify0(inArr, datingDataMat, datingLabels, 1)
+    # print(classifierResult)
     return classifierResult
